a24c193c8e6b70e48418c4a3daca53b1f4ba9a99
Harshitdev2151 <harshit.kumar2071@gmail.com>
2024-03-07 23:49:55 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Harshit Kumar on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
